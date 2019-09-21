4821bdd53234b1a723ee92f7593be3ef111766ce
Michelle Wong <michellekwong2@gmail.com>
2019-09-21 19:16:47 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -6,7 +6,7 @@ def meal_choice(protein = "meat", veg1, veg2)
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
-meal_choice(protein = "meat", veg1, veg2)
+meal_choice("meat", veg1, veg2)
 
 def meal_choice (protein = "meat", veg1, veg2)
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
